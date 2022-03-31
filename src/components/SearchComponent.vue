c1b252cf7434dd81e8e0e2a6315c0081eeb1af30
luc0liv <luoliveiradacunha@gmail.com>
2022-03-31 16:43:49 -0300

    Aplicando vue-loading-overlay e criando arquivo separado p/ o estilo do componente

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <loading
+      v-model:active="loading"
+      :can-cancel="true"
+      :is-full-page="true"
+      :color="spinnerColor"
+      :background-color="spinnerOverlayColor"
+    />
     <div class="search-wrapper">
       <input
         type="text"
@@ -7,7 +14,7 @@
         placeholder="Pesquisa"
         class="input-style"
       />
-      <button @click="gettingProducts" class="button-style">Buscar</button>
+      <button @click="searchProducts" class="button-style">Buscar</button>
     </div>
     <div v-if="products && products.length" class="products-list">
       <div v-for="(product, index) in products" :key="index" class="product">
@@ -19,16 +26,26 @@
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-class-component";
+import { Vue, Options } from "vue-class-component";
 import { getProducts } from "../api/products";
 import { Product } from "../interfaces/products";
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
 
+@Options({ components: { loading: Loading } })
 export default class SearchComponent extends Vue {
   searchTerm = "";
   products: Product[] = [];
   loading = false;
+  spinnerColor = "#a551d3";
+  spinnerOverlayColor = "#c5a2d370";
 
-  async gettingProducts() {
+  mounted() {
+    this.searchTerm = "camisa";
+    this.searchProducts();
+  }
+
+  async searchProducts() {
     if (this.searchTerm === "") {
       alert("Preencha o campo de pesquisa");
       return;
@@ -46,69 +63,6 @@ export default class SearchComponent extends Vue {
   }
 }
 </script>
-<style lang="scss" scoped>
-.search-wrapper {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  gap: 0px 10px;
-  padding: 2rem;
-  margin-bottom: 20px;
-
-  .input-style {
-    border: 0;
-    border-radius: 4px;
-    padding: 8px 10px;
-    font-size: 0.9rem;
-    background: #c5a2d370;
-    width: 20%;
-  }
-  .input-style::placeholder {
-    color: #a551d3;
-  }
-  .button-style {
-    color: #a551d3;
-    border: 1px solid #a551d3;
-    border-radius: 4px;
-    background-color: transparent;
-    padding: 8px 20px;
-  }
-
-  .button-style:hover {
-    -webkit-box-shadow: 5px 5px 20px -12px rgba(0, 0, 0, 0.25);
-    box-shadow: 5px 5px 20px -12px rgba(0, 0, 0, 0.25);
-  }
-}
-
-.products-list {
-  background: #c5a2d3;
-  background: -moz-linear-gradient(
-    45deg,
-    #c5a2d3 0%,
-    #a47aa0 56%,
-    #c56ebf 100%
-  );
-  background: -webkit-linear-gradient(
-    45deg,
-    #c5a2d3 0%,
-    #a47aa0 56%,
-    #c56ebf 100%
-  );
-  background: linear-gradient(45deg, #c5a2d3 0%, #a47aa0 56%, #c56ebf 100%);
-
-  border-radius: 8px;
-  margin: 0 auto;
-  padding: 2rem;
-  min-width: 30%;
-  width: max-content;
-
-  .product {
-    background-color: #ffffff94;
-    border-radius: 4px;
-    color: #a551d3;
-    margin-bottom: 8px;
-    padding: 10px;
-  }
-}
+<style lang="scss">
+@import "@/assets/scss/search.scss";
 </style>
